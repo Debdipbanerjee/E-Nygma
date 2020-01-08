09e5e17cfa4982357169ce65a456528cb4dfc847
Debdip Banerjee <44172289+Debdipbanerjee@users.noreply.github.com>
2020-01-09 00:46:24 +0530

    First

---
+++ E-Nygma.cpp
@@ -0,0 +1,119 @@
+/* Made By Debdip Banerjee on 3rd November,2019 */
+
+/*Last Update 8th January,2020*/
+
+/*Possible Update:
+1.Add Display Function (Done)
+2.Add PlayFair Cipher
+3.Random Key Generation
+*/
+
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class enygma 
+{
+private:
+	string plainText;
+	string cipherText;
+	int key = 0;
+
+public:
+	void Encrypt();
+	void Decrypt();
+	void get_Data();
+	void display_Data(string);
+	void display();
+};
+
+void enygma::get_Data()
+{
+	cout << "Please Enter a text:" << endl;
+	getline(cin, plainText);
+	cout << "enter a key:" << endl;
+	cin >> key;
+}
+
+void enygma::display_Data(string cipher)
+{
+	cout << "Plain Text: " << plainText << endl;
+	cout << "Key: " << key << endl;
+	cout << "Cipher Text: " << cipher << endl;
+}
+
+void enygma::display()
+{
+	cout << "_____________________________________________" << endl;
+	cout << "                  E-Nygma:                   " << endl;
+	cout << " A Fun Game Based on Caeser Cipher Technique " << endl;
+	cout << "_____________________________________________" << endl;
+	cout << endl;
+}
+
+void enygma::Encrypt()
+{
+	get_Data();
+	string p_text = plainText;
+	int k = key;
+	string result = "";
+	for (int i = 0; i < p_text.length(); i++)
+	{
+		if (isupper(p_text[i]))
+		{
+			result += char(int(p_text[i] + k - 65) % 26 + 65);
+		}
+		else
+		{
+			result += char(int(p_text[i] + k - 97) % 26 + 97);
+		}
+	}
+	display_Data(result);
+}
+
+void enygma::Decrypt()
+{
+	get_Data();
+	string p_text = plainText;
+	int k = key;
+	string result = "";
+	for (int i = 0; i < p_text.length(); i++)
+	{
+		if (isupper(p_text[i]))
+		{
+			result += char(int(p_text[i] - k - 65) % 26 + 65);
+		}
+		else
+		{
+			result += char(int(p_text[i] - k - 97) % 26 + 97);
+		}
+	}
+	display_Data(result);
+}
+
+int main()
+{
+	enygma e;
+	e.display();
+	char i;
+	cout << "Encrypt/Decrypt(E/D): ";
+	cin >> i;
+	cin.ignore();
+	switch (toupper(i))
+	{
+	case 'E': 
+		e.Encrypt();
+		break;
+	case 'D':
+		e.Decrypt();
+		break;
+	default:
+		cout << "Please try again, sorry";
+		break;
+	}
+	
+	return 0;
+}
+
---
+++ Readme_First.txt
@@ -0,0 +1,44 @@
+1.Name:	E-Nygma
+2.Developer: Debdip Banerjee
+3.Date: 3rd November,2019
+4.Type:	Caeser Cipher based Cryptographic Converter.
+
+5.Requirements:
+		Windows:  
+			1.Please Use Visual Studio Community 2018 Or Higher
+			2.Main.cpp ,add this File Under Same Project
+			3.Run From E-Nygma.cpp
+			4.Use VS Community 2018 Or Higher
+		
+		   Mac:
+			1.Please Use XCODE
+			2.Main.cpp ,add this File Under Same Project
+			3.Run From E-Nygma.cpp
+			
+6.Concept:
+		This a Cryptographic converter Using Caeser Cipher technique To Encrypt & Decrypt
+		Using Given Key.
+
+7.Rules:	1.Player Has to choose Encrypt Or Decrypt.
+		2.Enter the String.
+		3.Enter the Key.
+
+8.Inspiration: Got This Idea During 7th Sem Cryptography exam.
+
+9.Outcome:	1.Object Oriented Programming
+		2.Caeser Cipher Method implementation
+
+10.Personal Info:	
+		I,Debdip Banerjee am A Computer Science Undergrad At the Time Of making this Game.
+		I am Also A Game Programmer,Software Engineer,Technical Animator and Entreprenuer.
+		If Anyone Wants to use Any part Of My Source Code They can.
+		I am NOT responsible for ANY FUTURE use of my Code at any Work.
+		if anyone want to make any improvement they can,
+		if You want to contact me,Details are Below.
+
+11.Contact Info:
+		Email: Debdipbanerjee7@gmail.com
+		Whatsapp Number: 9007480046
+		Contact Number: +91-9007480046
+		LinkedIn Id: www.linkedin.com/in/debdip-banerjee-07
+		Facebook Id: http://facebook.com/Debdipbanerjee23
